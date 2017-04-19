9175ef1ba2093feebd3ec005751bb1cb8f912c43
Dan Palmer <dan@danpalmer.me>
2017-04-19 22:49:58 +0100

    Split entrypoints

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './_js/index.js',
+  entry: {
+    index: './_js/index.js',
+  },
   output: {
     path: path.resolve(__dirname, './assets'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   }
 };
