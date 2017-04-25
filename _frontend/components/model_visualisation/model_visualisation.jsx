de7f3b7962b3ce470c75d0a4a4c9c43ce924bb07
Dan Palmer <dan@danpalmer.me>
2017-04-25 21:16:45 +0100

    Link up a React component for this

---
+++ _frontend/components/model_visualisation/model_visualisation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class Model extends React.Component {
+
+  render() {
+    return (
+      <div className="model">Model coming soon...</div>
+    );
+  }
+}
+
+Model.propTypes = {
+};
--- _frontend/index.js
+++ _frontend/index.js
@@ -4,11 +4,13 @@ import 'document-register-element';
 import 'reactive-elements';
 
 import Logo from './components/logo/logo';
+import Model from './components/model_visualisation/model_visualisation';
 import ConstituencyMap from './components/constituency_map/constituency_map';
 import randomiseLists from './utils/randomise_lists';
 
 
 randomiseLists();
 
+document.registerReact('sixfifty-model', Model);
 document.registerReact('sixfifty-logo', Logo);
 document.registerReact('sixfifty-map', ConstituencyMap);
