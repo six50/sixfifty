4536fca967664ae01d0da9923238c9bf246d7e54
Dan Palmer <dan@danpalmer.me>
2017-04-25 22:19:39 +0100

    Component to plug together data and visualisation

--- _frontend/components/model_visualisation/model_visualisation.jsx
+++ _frontend/components/model_visualisation/model_visualisation.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ConstituencyMap from '../constituency_map/constituency_map';
+
+const MODEL_PARTY_TRANSLATIONS = {
+  'Lab': 'lab',
+  'C': 'con',
+  'LD': 'ld',
+};
 
 export default class Model extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.predictions = {};
+
+    for (let prediction of props.predictions) {
+      let winner = MODEL_PARTY_TRANSLATIONS[prediction.winner] || 'oth';
+      let name = prediction.constituency
+        .replace(/\&/g, 'and')
+        .replace(/\s+/g, '_')
+        .replace(/_+/g, '_')
+        .replace(',', '');
+
+      this.predictions[name] = winner;
+    }
+  }
 
   render() {
     return (
-      <div className="model">Model coming soon...</div>
+      <div className="model">
+        <ConstituencyMap
+          constituencies={this.predictions}
+          colours={this.props.colours}
+        />
+      </div>
     );
   }
 }
 
 Model.propTypes = {
+  predictions: PropTypes.array,
+  colours: PropTypes.object
 };
