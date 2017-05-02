0ba27e00c33438930700ce4f45ecab124fc127b4
Dan Palmer <dan@danpalmer.me>
2017-05-03 00:15:28 +0100

    Drop the Victory based visualisation for now

--- _frontend/index.js
+++ _frontend/index.js
@@ -8,7 +8,6 @@ import Logo from './components/logo/logo';
 import Model from './components/model_visualisation/model_visualisation';
 import Countdown from './components/countdown/countdown';
 import ConstituencyMap from './components/constituency_map/constituency_map';
-// import PollsVisualisation from './components/polls_visualisation/polls_visualisation';
 
 // D3
 import pollsTracker from './d3/polls_tracker';
@@ -24,7 +23,6 @@ document.registerReact('sixfifty-model', Model);
 document.registerReact('sixfifty-logo', Logo);
 document.registerReact('sixfifty-countdown', Countdown);
 document.registerReact('sixfifty-map', ConstituencyMap);
-// document.registerReact('sixfifty-polls', PollsVisualisation);
 
 // Initialise D3 visualisations
 pollsTracker(document.querySelector('#polls-tracker'));
