4149d977c183497efbe5b30f68f885a7f543a367
Dan Palmer <dan@danpalmer.me>
2017-05-06 15:36:33 +0100

    Update polls list to use new data

--- _frontend/components/polls_list/polls_list.jsx
+++ _frontend/components/polls_list/polls_list.jsx
@@ -10,6 +10,9 @@ const DATA_NAME_MAP = Object.entries({con: 'con', lab: 'lab', lib: 'ld', ukip: '
 
 export default class Model extends React.Component {
   renderDateRange(from, to) {
+    if (!from) {
+      return dateFormat(to, 'mmm d');
+    }
     if (from.getMonth() == to.getMonth()) {
       if (from.getDate() == to.getDate()) {
         // Just one day, render `Apr 29`
@@ -39,28 +42,31 @@ export default class Model extends React.Component {
               <th><div>Sample&nbsp;Size</div></th>
               <th><div>Method</div></th>
               <th></th>
-              {DATA_NAME_MAP.map(([_, nameKey], key) => (
-                <th key={key}><div>{this.props.names[nameKey]}</div></th>
+              {this.props.parties.map((partyKey, key) => (
+                <th key={key}><div>{this.props.names[partyKey]}</div></th>
               ))}
             </tr>
           </thead>
           <tbody>
             {this.props.polls.slice(0).reverse().map((poll, key) => (
-              <tr key={key}>
-                <td>{this.renderDateRange(poll.sampled_from, poll.sampled_to)}</td>
-                <td title={`Published by ${poll.publisher}`}>{poll.pollster}</td>
-                <td>{poll.sample_size.toFixed(0)}</td>
+              <tr key={key} className="poll">
+                <td>{this.renderDateRange(poll.from, poll.to)}</td>
+                <td title={`Published by ${poll.client}`}>{poll.company}</td>
+                <td>{poll.sample_size ? poll.sample_size.toFixed(0) : '—'}</td>
                 <td>{poll.method}</td>
                 <td>
                   {poll.pdf && <a className='doc-link' href={poll.pdf}></a>}
                 </td>
-                {DATA_NAME_MAP.map(([partyKey, colourKey], key2) => (
+                {this.props.parties.map((partyKey, key2) => (
                   <td
                     key={key2}
                     className='poll-value'
-                    style={{backgroundColor: Color(this.props.colours[colourKey]).alpha(0.3)}}
+                    style={{backgroundColor: Color(this.props.colours[partyKey]).alpha(0.3)}}
                   >
-                    {(poll[partyKey] * 100.0).toFixed(1)}
+                    {poll[partyKey] ?
+                      (poll[partyKey] * 100.0).toFixed(1) + '%'
+                      : '—'
+                    }
                   </td>
                 ))}
               </tr>
@@ -75,5 +81,6 @@ export default class Model extends React.Component {
 Model.propTypes = {
   names: PropTypes.object,
   colours: PropTypes.object,
+  parties: PropTypes.array,
   polls: PropTypes.array
 };
