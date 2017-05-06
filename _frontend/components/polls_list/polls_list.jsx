2ae20dc82cbc2c01d1f6f19c9e479c01b3e4207a
Dan Palmer <dan@danpalmer.me>
2017-05-06 12:20:27 +0100

    Add date formatting

--- _frontend/components/polls_list/polls_list.jsx
+++ _frontend/components/polls_list/polls_list.jsx
@@ -3,13 +3,29 @@ import './polls_list.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Color from 'color';
+import dateFormat from 'dateformat';
 
 const DATA_NAME_MAP = Object.entries({con: 'con', lab: 'lab', lib: 'ld', ukip: 'ukip', green: 'grn'});
 
 
 export default class Model extends React.Component {
   renderDateRange(from, to) {
-    return 'May 3-5';
+    if (from.getMonth() == to.getMonth()) {
+      if (from.getDate() == to.getDate()) {
+        // Just one day, render `Apr 29`
+        return dateFormat(to, 'mmm d');
+      } else {
+        // Same month, different days, render `Apr 29-30`
+        const left = dateFormat(from, 'mmm d');
+        const right = to.getDate();
+        return `${left}–${right}`;
+      }
+    } else {
+      // Different months, render `Apr 29 - May 1`
+      const left = dateFormat(from, 'mmm d');
+      const right = dateFormat(to, 'mmm d');
+      return `${left} – ${right}`;
+    }
   }
 
   render() {
@@ -20,21 +36,19 @@ export default class Model extends React.Component {
             <tr>
               <th><div>Dates</div></th>
               <th><div>Pollster</div></th>
-              <th><div>Publisher</div></th>
               <th><div>Sample&nbsp;Size</div></th>
               <th><div>Method</div></th>
-              <th><div>Links</div></th>
+              <th></th>
               {DATA_NAME_MAP.map(([_, nameKey], key) => (
                 <th key={key}><div>{this.props.names[nameKey]}</div></th>
               ))}
             </tr>
           </thead>
           <tbody>
-            {this.props.polls.map((poll, key) => (
+            {this.props.polls.slice(0).reverse().map((poll, key) => (
               <tr key={key}>
                 <td>{this.renderDateRange(poll.sampled_from, poll.sampled_to)}</td>
-                <td>{poll.pollster}</td>
-                <td>{poll.publisher}</td>
+                <td title={`Published by ${poll.publisher}`}>{poll.pollster}</td>
                 <td>{poll.sample_size.toFixed(0)}</td>
                 <td>{poll.method}</td>
                 <td>
